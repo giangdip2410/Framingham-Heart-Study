bd74262097af6d5c7ae3b5dbe05d836a9c59bfef
giangdip2410 <truonggiang.tmb@gmail.com>
2021-05-15 15:14:35 +0700

    change something

--- app.py
+++ app.py
@@ -67,10 +67,18 @@ def get_predict(data):
 
 app = Flask(__name__)  # Initialize the flask App
 
+# Read the mailgun secret key from environment variables
+mailgun_secret_key_value = os.environ.get("MAILGUN_SECRET_KEY", None)
+
+# This is needed for Heroku configuration, as in Heroku our
+# app will probably not run on port 5000, as Heroku will automatically
+# assign a port for our application
+port = int(os.environ.get("PORT", 5000))
+
 
 @app.route("/")  # Homepage
 def home():
-    return render_template("index.html")
+    return render_template("index.html", value=mailgun_secret_key_value)
 
 
 @app.route("/predict", methods=["POST"])
@@ -99,7 +107,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.debug = False
-    from werkzeug.serving import run_simple
-
-    run_simple("localhost", 5000, app)
+    app.run("0.0.0.0", port=port, debug=True)
